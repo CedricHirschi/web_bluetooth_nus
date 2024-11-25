d421a0b880a0a1acdfea1ff5d352ee0a61ba6c6c
Cedric Hirschi <cedr02@live.com>
2024-11-25 15:29:54 +0100

    Move code to Bluetooth NUS handler

--- index.js
+++ index.js
@@ -6,144 +6,85 @@ const outputReceive = document.getElementById('receive-text');
 const statusLabel = document.getElementById('status-label');
 const warningBanner = document.getElementById('warning-banner');
 
-const nordicUARTService = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-const nordicUARTTXCharacteristic = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
-const nordicUARTRXCharacteristic = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+const bluetoothHandler = new BluetoothNUSHandler();
 
-let currentDevice = null;
-let uartService = null;
-let uartTxCharacteristic = null;
-let uartRxCharacteristic = null;
-
-function manageConnection() {
-    (currentDevice === null) ? manageConnectionConnect() : manageConnectionDisconnect();
-}
-
-async function manageConnectionConnect() {
-    statusLabel.textContent = 'Connecting...';
-
-    try {
-        const device = await navigator.bluetooth.requestDevice({
-            filters: [{ services: [nordicUARTService] }]
-        });
-
-        await device.gatt.connect();
-
-        uartService = await device.gatt.getPrimaryService(nordicUARTService);
-        uartTxCharacteristic = await uartService.getCharacteristic(nordicUARTTXCharacteristic);
-        uartRxCharacteristic = await uartService.getCharacteristic(nordicUARTRXCharacteristic);
-
-        currentDevice = device;
-
-        btnConnect.textContent = 'Disconnect';
-        statusLabel.textContent = 'Connected to ' + device.name;
-
-        btnReceive.disabled = false;
-        btnSend.disabled = false;
-        inputSend.disabled = false;
-    } catch (error) {
-        btnConnect.textContent = 'Connect';
-        statusLabel.textContent = 'No device selected';
-
-        if (error.name === 'NotFoundError') return;
-
-        await manageConnectionDisconnect();
-
-        console.warn('Connection failed:', error);
-        statusLabel.textContent = 'Connection failed: ' + error;
-    }
-}
-
-async function manageConnectionDisconnect() {
-    statusLabel.textContent = 'Disconnecting...';
-
-    if (currentDevice !== null) await currentDevice.gatt.disconnect();
-
-    currentDevice = null;
-
-    btnConnect.textContent = 'Connect';
-    statusLabel.textContent = 'Disconnected';
-
-    btnReceive.disabled = true;
-    btnSend.disabled = true;
-    inputSend.disabled = true;
-}
-
-btnConnect.addEventListener('click', manageConnection);
-
-
-async function rxHandler(event) {
-    const value = event.target.value;
-    const text = new TextDecoder().decode(value);
-
-    outputReceive.textContent += text;
-}
-
-
-async function manageReception() {
-    if (currentDevice === null || uartRxCharacteristic === null) return;
-
-    if (btnReceive.textContent === 'Stop receiving') {
-        await uartRxCharacteristic.stopNotifications();
-        uartRxCharacteristic.removeEventListener('characteristicvaluechanged', rxHandler);
-
-        btnReceive.textContent = 'Start receiving';
-    } else {
-        await uartRxCharacteristic.startNotifications();
-        uartRxCharacteristic.addEventListener('characteristicvaluechanged', rxHandler);
-
-        btnReceive.textContent = 'Stop receiving';
-    }
-}
-
-btnReceive.addEventListener('click', manageReception);
-
-
-async function manageSending() {
-    if (currentDevice === null) return;
-
-    const text = inputSend.value;
-    const data = new TextEncoder().encode(text);
-
-    try {
-        await uartTxCharacteristic.writeValue(data);
-    } catch (error) {
-        console.warn('Falling back to minimum MTU:', error);
-
-        const MTU = 20;
-
-        for (let i = 0; i < data.length; i += MTU) {
-            await uartTxCharacteristic.writeValueWithoutResponse(data.slice(i, i + MTU));
+btnConnect.addEventListener('click', async () => {
+    if (!await bluetoothHandler.isConnected()) {
+        statusLabel.textContent = 'Connecting...';
+        try {
+            const deviceName = await bluetoothHandler.connect();
+            if (deviceName) {
+                btnConnect.textContent = 'Disconnect';
+                statusLabel.textContent = 'Connected to ' + deviceName;
+                btnReceive.disabled = false;
+                btnSend.disabled = false;
+                inputSend.disabled = false;
+            } else {
+                statusLabel.textContent = 'No device selected';
+            }
+        } catch (error) {
+            statusLabel.textContent = 'Connection failed: ' + error;
+            await bluetoothHandler.disconnect();
         }
+    } else {
+        statusLabel.textContent = 'Disconnecting...';
+        await bluetoothHandler.disconnect();
+        btnConnect.textContent = 'Connect';
+        statusLabel.textContent = 'Disconnected';
+        btnReceive.disabled = true;
+        btnSend.disabled = true;
+        inputSend.disabled = true;
     }
-}
+});
 
-btnSend.addEventListener('click', manageSending);
+btnSend.addEventListener('click', async () => {
+    const text = inputSend.value;
+    await bluetoothHandler.send(text);
+});
+
+btnReceive.addEventListener('click', async () => {
+    if (btnReceive.textContent === 'Disable Reception') {
+        await bluetoothHandler.stopNotifications();
+        btnReceive.textContent = 'Enable Reception';
+    } else {
+        await bluetoothHandler.startNotifications((data) => {
+            outputReceive.textContent += data;
+        });
+        btnReceive.textContent = 'Disable Reception';
+    }
+});
 
 // Check connection every second
-setInterval(() => {
-    if (currentDevice === null) return;
-
-    currentDevice.gatt.getPrimaryService(nordicUARTService)
-        .then(() => statusLabel.textContent = 'Connected to ' + currentDevice.name)
-        .catch(() => {
-            if (btnReceive.textContent === 'Stop receiving') manageReception();
-            manageConnectionDisconnect()
-        });
+setInterval(async () => {
+    if (await bluetoothHandler.isConnected()) {
+        statusLabel.textContent = 'Connected to ' + bluetoothHandler.deviceName;
+    } else {
+        if (btnReceive.textContent === 'Disable Reception') {
+            await bluetoothHandler.stopNotifications();
+            btnReceive.textContent = 'Enable Reception';
+        }
+        btnConnect.textContent = 'Connect';
+        statusLabel.textContent = 'Disconnected';
+        btnReceive.disabled = true;
+        btnSend.disabled = true;
+        inputSend.disabled = true;
+    }
 }, 1000);
 
 // On website load, check if Bluetooth is available
 if (!('bluetooth' in navigator)) {
-    // disable all content, add banner to inform user
     warningBanner.style.display = 'block';
     btnConnect.disabled = true;
     statusLabel.textContent = 'Bluetooth not available';
 
-    // get os of user
     const user = navigator.userAgent;
-    const os = user.includes('Android') ? 'Android' : user.includes('iPhone') || user.includes('iPad') ? 'iOS' : user.includes('Linux') ? 'Linux' : user.includes('Windows') ? 'Windows' : user.includes('Mac') ? 'MacOS' : 'Unknown';
+    const os = user.includes('Android') ? 'Android' :
+               user.includes('iPhone') || user.includes('iPad') ? 'iOS' :
+               user.includes('Linux') ? 'Linux' :
+               user.includes('Windows') ? 'Windows' :
+               user.includes('Mac') ? 'MacOS' : 'Unknown';
 
-    warningBanner.innerHTML = `<strong>Warning!</strong> Bluetooth is not available in this session.<br><br>`;
+    warningBanner.innerHTML = `<strong>Warning!</strong> Bluetooth is not available in this session.<br>`;
 
     switch (os) {
         case 'Android':
@@ -153,9 +94,8 @@ if (!('bluetooth' in navigator)) {
             warningBanner.innerHTML += `On iOS, please use Safari.`;
             break;
         case 'Linux':
-            warningBanner.innerHTML += `On Linux, please use Chrome.<br><br>`;
-            warningBanner.innerHTML += `You may need to enable the flag "Experimental Web Platform features" by entering the following URL into your browser's address bar and setting the flag to <strong>Enabled</strong>:<br><code>chrome://flags/#enable-experimental-web-platform-features</code><br>`;
-            warningBanner.innerHTML += `You may need to enable the flag "Web Bluetooth New Permissions Backend" by entering the following URL into your browser's address bar and setting the flag to <strong>Enabled</strong>:<br><code>chrome://flags/#enable-web-bluetooth-new-permissions-backend</code>`;
+            warningBanner.innerHTML += `On Linux, please use Chrome.<br>`;
+            warningBanner.innerHTML += `You may need to enable the flag "Experimental Web Platform features" by entering <code>chrome://flags/#enable-experimental-web-platform-features</code> into your browser's address bar as well as "Web Bluetooth New Permissions Backend" by entering <code>chrome://flags/#enable-web-bluetooth-new-permissions-backend</code>.`;
             break;
         case 'Windows':
             warningBanner.innerHTML += `On Windows, please use Chrome.`;
@@ -163,9 +103,7 @@ if (!('bluetooth' in navigator)) {
         case 'MacOS':
             warningBanner.innerHTML += `On MacOS, please use Chrome.`;
             break;
-    
         default:
             break;
     }
-
 }
---
+++ nus.js
@@ -0,0 +1,93 @@
+class BluetoothNUSHandler {
+    constructor() {
+        this.nordicUARTService = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+        this.nordicUARTTXCharacteristic = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+        this.nordicUARTRXCharacteristic = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+
+        this.currentDevice = null;
+        this.uartService = null;
+        this.uartTxCharacteristic = null;
+        this.uartRxCharacteristic = null;
+    }
+
+    get deviceName() {
+        return this.currentDevice ? this.currentDevice.name : null;
+    }
+
+    async connect() {
+        if (this.isConnected()) return this.currentDevice.name;
+
+        try {
+            const device = await navigator.bluetooth.requestDevice({
+                filters: [{ services: [this.nordicUARTService] }]
+            });
+
+            const server = await device.gatt.connect();
+            this.uartService = await server.getPrimaryService(this.nordicUARTService);
+            this.uartTxCharacteristic = await this.uartService.getCharacteristic(this.nordicUARTTXCharacteristic);
+            this.uartRxCharacteristic = await this.uartService.getCharacteristic(this.nordicUARTRXCharacteristic);
+
+            this.currentDevice = device;
+            
+            return this.deviceName;
+        } catch (error) {
+            this.currentDevice = null;
+            this.uartService = null;
+            this.uartTxCharacteristic = null;
+            this.uartRxCharacteristic = null;
+
+            if (error.name === 'NotFoundError') return this.deviceName;
+
+            throw error;
+        }
+    }
+
+    async disconnect() {
+        if (!this.isConnected()) return;
+
+        await this.currentDevice.gatt.disconnect();
+
+        this.currentDevice = null;
+        this.uartService = null;
+        this.uartTxCharacteristic = null;
+        this.uartRxCharacteristic = null;
+    }
+
+    async send(text) {
+        if (!this.isConnected()) throw Error('Device is not connected');
+
+        const data = new TextEncoder().encode(text);
+
+        try {
+            await this.uartTxCharacteristic.writeValue(data);
+        } catch (error) {
+            // Fallback for devices with limited MTU
+            const MTU = 20; // 20 MTU should be supported by all devices
+            for (let i = 0; i < data.length; i += MTU) {
+                await this.uartTxCharacteristic.writeValueWithoutResponse(data.slice(i, i + MTU));
+            }
+        }
+    }
+
+    async startNotifications(callback) {
+        if (!this.isConnected()) throw Error('Device is not connected');
+        if (!callback) throw Error('Callback is required');
+
+        await this.uartRxCharacteristic.startNotifications();
+        this.uartRxCharacteristic.addEventListener('characteristicvaluechanged', (event) => {
+            const value = event.target.value;
+            const text = new TextDecoder().decode(value);
+            callback(text);
+        });
+    }
+
+    async stopNotifications() {
+        if (!this.isConnected()) throw Error('Device is not connected');
+
+        await this.uartRxCharacteristic.stopNotifications();
+    }
+
+    isConnected() {
+        return this.currentDevice && this.currentDevice.gatt.connected;
+    }
+}
